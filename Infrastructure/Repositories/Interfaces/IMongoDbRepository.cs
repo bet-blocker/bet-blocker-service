8f94b63fc9f17e815e4b0a5aca82fc66029c46c7
RobsonTrasel <robson.trasel@gmail.com>
2024-11-10 22:39:47 -0300

    chore: create interface for mongo repository types

---
+++ Infrastructure/Repositories/Interfaces/IMongoDbRepository.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface IMongoDbRepository
+{
+    Task InsertDocumentAsync(BsonDocument document);
+    Task<List<BsonDocument>> GetAllDocumentsAsync();
+}
